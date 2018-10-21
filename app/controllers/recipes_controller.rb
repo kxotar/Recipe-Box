8463e25f8fb453d415d0a25fe05989c5546c44a3
Rakesh Sharma <rakesh@clearfunds.com>
2018-10-21 23:23:27 +0530

    auth added

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,49 @@
+class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  def index
+    @recipes = Recipe.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def edit
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: "Successfully created a new Recipe."
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: "Successfully updated."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: "Successfully Destroyed Recipe."
+  end
+
+  private
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy],
+                                   directions_attributes: [:id, :step, :_destroy])
+  end
+end
